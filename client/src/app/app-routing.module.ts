c9bbf6d9ba182836f80e77b1402c01b4003a5abb
Isabelle Hjelden <hjeld013@morris.umn.edu>
2021-03-14 23:10:22 -0800

    Delete user.service.mock and update app-routing
    Needed to update 'WordpackComponent' to 'WordPackComponent' and delete ContextpackProfileComponent because it is no longer needed.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContextpackListComponent } from './contextpack/contextpack-list.component';
-import { ContextpackProfileComponent } from './contextpack/contextpack-profile.component';
-import { WordpackComponent } from './wordpack/wordpack.component';
+import { WordPackComponent } from './wordpack/wordpack.component';
 
 const routes: Routes = [
   {path: '', component: ContextpackListComponent},
   {path: 'contextpacks', component: ContextpackListComponent},
-  {path: 'contextpacks/:id', component: ContextpackProfileComponent},
-  {path: 'wordpacks', component: WordpackComponent },
+  {path: 'wordpacks', component: WordPackComponent },
 ];
 
 @NgModule({
