f1768b005aa59f1535ae5713cb15b6c5d6d52c41
Noah <const112@umn.edu>
2021-04-01 14:35:11 -0500

    Restart Client-Side Logic For Adding Word List
    Restarted the logic implementation on the client-side for adding the word list. Needs methods to make wordlist word/forms arrays and insert new words/forms appropriately.

--- client/src/app/add-wordlists/add-wordlists.component.ts
+++ client/src/app/add-wordlists/add-wordlists.component.ts
@@ -13,145 +13,50 @@ import { ContextpackService } from '../contextpack/contextpack.service';
 })
 export class AddWordlistsComponent implements OnInit {
 
-  addWordPackForm: FormGroup;
-  shown = false;
+  addWordListForm: FormGroup;
+  contextPack: ContextPack;
   wordPack: WordPack;
-  contextpack: ContextPack;
-  enabled = true;
-  cpId: string;
+  word: Words;
 
-  formErrors = {
-    wordpacks: this.wordPacksErrors()
+  addWLValidation = {
+    name: [
+      {type: 'required', message: 'Name is required'},
+      {type: 'minlength', message: 'Name must be at least 2 characters long'},
+      {type: 'maxlength', message: 'Name cannot be longer than 50 characters'},
+    ],
+    enabled: [
+      {type: 'required', message: 'Enabled/Disabled is required'},
+      {type: 'pattern', message: 'Must be either enabled or disabled'},
+    ],
   };
 
-  addContextPackValidationMessages = {
-    wordPacks: {
-      name: [
-        {type: 'required', message: 'Name is required'}
-      ],
-      enabled: {required: 'Must be true or false'},
-      nouns: { word: {}, forms: {}},
-      verbs: { word: {}, forms: {}},
-      adjectives: { word: {}, forms: {}},
-      misc: { word: {}, forms: {}}
-    }
-  };
-
-  constructor(private fb: FormBuilder, private contextPackService: ContextpackService,
-    private snackBar: MatSnackBar, private router: Router) { }
+  constructor(private contextPackService: ContextpackService, private fb: FormBuilder,
+     private snackBar: MatSnackBar, private router: Router) { }
 
     createForms() {
-      this.addWordPackForm = this.fb.group({
-        wordPacks: this.fb.array([])
-      });
-      this.addWordPackForm.valueChanges.subscribe(data => this.validateForm());
-    }
-
-  ngOnInit(): void {
-    this.createForms();
-  }
-
-  initWordPack() {
-    return this.fb.group({
-      name: new FormControl('', Validators.compose([
-        Validators.required,
-      ])),
-      enabled: new FormControl('true', Validators.compose([
-        Validators.required,
-        Validators.pattern('^(true|false)$'),
-      ])),
-      nouns: this.fb.array([]),
-      adjectives: this.fb.array([]),
-      verbs: this.fb.array([]),
-      misc: this.fb.array([])
-
-    });
-  }
-
-  initWords() {
-    return this.fb.group({
-      word: [''],
-      forms: this.fb.array([
-         this.fb.control('')
-      ])
-    });
-  }
-
-  addWordPack() {
-    const control = this.addWordPackForm.controls.wordPacks as FormArray;
-    control.push(this.initWordPack());
-  }
-
-  addPosArray(ix: number, pos: string){
-    const control = (this.addWordPackForm.controls.wordPacks as FormArray).at(ix).get(`${pos}`) as FormArray;
-    control.push(this.initWords());
-  }
-
-  addForms(ix: number, iy: number, pos: string) {
-    const control = ((this.addWordPackForm.controls.wordPacks as FormArray).at(ix).get(`${pos}`) as FormArray)
-    .at(iy).get('forms') as FormArray;
-    control.push(this.fb.control(''));
-  }
-
-  setWord(ix: number, iy: number, pos: string){
-    const control = ((this.addWordPackForm.controls.wordPacks as FormArray).at(ix).get(`${pos}`) as FormArray)
-    .at(iy).get('forms') as FormArray;
-
-    const formAdd = ((this.addWordPackForm.controls.wordPacks as FormArray).at(ix).get(`${pos}`) as FormArray).at(iy).get('word');
-    console.log('didn\'t go through');
-    if(control.getRawValue()[0] !== formAdd.value  ){
-      control.insert(0,formAdd);
-      console.log(ix,iy);
-    }
-  }
-
-  validateForm(){
-    this.validateWordPacks();
-  }
-
-  validateWordPacks() {
-    const wordPacks = this.addWordPackForm.controls.wordPacks as FormArray;
-    this.formErrors.wordpacks = [];
-    for (let index = 1; index <= wordPacks.length; index++) {
-      this.formErrors.wordpacks.push({
-        name: [' ', [Validators.required]],
-        enabled: [' ', [Validators.required]],
-        nouns: [{
-          word: '',
-          forms: this.fb.array([
-            this.fb.control('')
-          ]),
-        }]
+      this.addWordListForm = this.fb.group({
+        name: new FormControl('', Validators.compose([
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(50)
+        ])),
+        enabled: new FormControl('', Validators.compose([
+          Validators.required,
+          Validators.pattern('^(false|False|True|true)$')
+        ]))
       });
     }
-  }
 
-  wordPacksErrors() {
-    return [{
-      name: [' ', [Validators.required]],
-      enabled: [' ', [Validators.required]],
-      nouns: this.wordsErrors(),
-    }];
-  };
+    ngOnInit(): void{
+      this.createForms();
+    }
 
-  wordsErrors() {
-    return [{
-      word: '',
-      forms: this.fb.array([this.fb.control('')])
-    }];
-  };
-
-  submitForm() {
-    this.contextPackService.addWordPack(this.addWordPackForm.value).subscribe(newID => {
-      this.snackBar.open('Added Pack ' + this.addWordPackForm.value.name, null, {
-        duration: 2000,
+    submitForm() {
+      this.contextPackService.addWordPack(this.addWordListForm.value).subscribe(newID => {
+        this.snackBar.open('Added Word List', null, { duration: 2000});
+        this.router.navigate(['/contextpacks/', newID]);
+      }, err => {
+        this.snackBar.open('Failed to add the todo', 'OK', { duration: 5000});
       });
-      this.router.navigate(['/contextpacks/', newID]);
-    }, err => {
-      this.snackBar.open(
-        'Failed to add the word pack (check that all required fields are filled in)', 'OK', {
-        duration: 5000,
-      });
-    });
-  }
+    }
 }
