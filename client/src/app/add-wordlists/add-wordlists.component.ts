659dea6a2290c2538affd86a93f931d6bd89c60a
Noah <const112@umn.edu>
2021-04-02 16:38:15 -0500

    Work on adding wordPacks
    Figured out controls in the form and got rid of errors pertaining to them. Working on iterating through wordpacks on page with *ngFor loop.

--- client/src/app/add-wordlists/add-wordlists.component.ts
+++ client/src/app/add-wordlists/add-wordlists.component.ts
@@ -19,37 +19,87 @@ export class AddWordlistsComponent implements OnInit {
   word: Words;
 
   addWLValidation = {
-    name: [
-      {type: 'required', message: 'Name is required'},
-      {type: 'minlength', message: 'Name must be at least 2 characters long'},
-      {type: 'maxlength', message: 'Name cannot be longer than 50 characters'},
-    ],
-    enabled: [
-      {type: 'required', message: 'Enabled/Disabled is required'},
-      {type: 'pattern', message: 'Must be either enabled or disabled'},
-    ],
+    wordPacks: {
+      name: [
+        {type: 'required', message: 'Name is required'},
+        {type: 'minlength', message: 'Name must be at least 2 characters long'},
+        {type: 'maxlength', message: 'Name cannot be longer than 50 characters'},
+      ],
+      enabled: [
+        {type: 'required', message: 'Enabled/Disabled is required'},
+        {type: 'pattern', message: 'Must be either enabled or disabled'},
+      ],
+    }
   };
 
   constructor(private contextPackService: ContextpackService, private fb: FormBuilder,
      private snackBar: MatSnackBar, private router: Router) { }
 
+    // createForms() {
+    //   this.addWordListForm = this.fb.group({
+    //         name: new FormControl('', Validators.compose([
+    //           Validators.required,
+    //           Validators.minLength(2),
+    //           Validators.maxLength(50)
+    //         ])),
+    //         enabled: new FormControl('', Validators.compose([
+    //           Validators.required,
+    //           Validators.pattern('^(false|False|True|true)$')
+    //         ])),
+    //         nouns: this.fb.array([]),
+    //         adjectives: this.fb.array([]),
+    //         verbs: this.fb.array([]),
+    //         misc: this.fb.array([]),
+    //     });
+    // }
+
     createForms() {
       this.addWordListForm = this.fb.group({
-        name: new FormControl('', Validators.compose([
-          Validators.required,
-          Validators.minLength(2),
-          Validators.maxLength(50)
-        ])),
-        enabled: new FormControl('', Validators.compose([
-          Validators.required,
-          Validators.pattern('^(false|False|True|true)$')
-        ]))
+        wordPacks: this.fb.array([])
       });
+      // this.addWordListForm.valueChanges.subscribe(data => this.validateForm());
     }
 
     ngOnInit(): void{
       this.createForms();
+      this.initWordPack();
     }
+
+    initWordPack() {
+      return this.fb.group({
+        name: new FormControl('', Validators.compose([
+          Validators.required,
+        ])),
+        enabled: new FormControl('true', Validators.compose([
+          Validators.required,
+          Validators.pattern('^(true|false)$'),
+        ])),
+        nouns: this.fb.array([]),
+        adjectives: this.fb.array([]),
+        verbs: this.fb.array([]),
+        misc: this.fb.array([])
+      });
+    }
+
+    initWords() {
+      return this.fb.group({
+        word: [''],
+        forms: this.fb.array([
+           this.fb.control('')
+        ])
+      });
+    }
+
+    addWordPack() {
+      const control = this.addWordListForm.controls.wordPacks as FormArray;
+      control.push(this.initWordPack());
+    }
+
+    addPosArray(ix: number, pos: string){
+      const control = (this.addWordListForm.controls.wordPacks as FormArray).at(ix).get(`${pos}`) as FormArray;
+      control.push(this.initWords());
+    }
+
 
     submitForm() {
       this.contextPackService.addWordPack(this.addWordListForm.value).subscribe(newID => {
